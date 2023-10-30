f32d08b5e48d833188a910eaa24ffa4ca93c437d
adityarai2 <adityarai232004@gmail.com>
2023-10-31 00:07:08 +0530

    write a program to check age eligibility for vote

---
+++ basic/vote.py
@@ -0,0 +1,11 @@
+print("enter your age=")
+age=int (input())
+if age >= 18:
+    print("you are an adult")
+    print("you casn vote")
+else:
+    
+    print("you are a minor")
+    print("you cannot vote ")
+
+print("thank you")
