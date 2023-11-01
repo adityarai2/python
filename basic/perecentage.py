953c5b4a83ca40d2f54f9293a29ad52497312310
adityarai2 <adityarai232004@gmail.com>
2023-11-01 17:18:38 +0530

    write a code for while loop

--- basic/perecentage.py
+++ basic/perecentage.py
@@ -1,1 +1,7 @@
-
+print("enter the marks of math")
+print("enter the marks of physics")
+print("enter the marks of chemistry:")
+marks=int("math,physics,chemistry")
+total=(math+physics+chemistry)
+percentage= int(total/300)*100
+print("your percentage =",percentage)
