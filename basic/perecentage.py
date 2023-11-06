775ec1c68e44a4e78e131437085242ff8c754d2a
adityarai2 <adityarai232004@gmail.com>
2023-11-07 00:22:01 +0530

    program to run a code by user defined function

--- basic/perecentage.py
+++
@@ -1,4 +0,0 @@
-marks=physics=76 , maths=78,chemistry =88
-total= ["physics"]+["chemistry"]+["maths"]
-percentage= int(total/300)*100
-print("your percentage =",percentage)
---
+++ basic/user.py
@@ -0,0 +1,3 @@
+def print_sum(first,seceond):
+    print(first+seceond)
+print_sum(7,8)
