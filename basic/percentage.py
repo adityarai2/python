0ea0f0c41bbbbfb6dcd89023664eb15f1ff5c96a
adityarai2 <adityarai232004@gmail.com>
2023-11-09 19:14:13 +0530

    program to calculate percentage

---
+++ basic/percentage.py
@@ -0,0 +1,11 @@
+marks1 = int(input("enter marks of physics="))
+marks2= int(input("enter the marks of maths="))
+marks3= int(input("enter the marks of chemistry"))
+percentage = ((marks1+marks2+marks3)/300)*100
+print("your percentage =",percentage)
+
+
+
+
+
+
