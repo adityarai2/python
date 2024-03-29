77b87c539bc872b033786618644ba7ef33acb261
adityarai2 <adityarai232004@gmail.com>
2024-03-29 10:10:03 +0530

    concatinate and to find length of string in python

---
+++ basic/concatinate.py
@@ -0,0 +1,14 @@
+str1 = "this information is "
+str2= "very useful"
+print(str1+str2)
+#other method
+str3 = "i am going "
+str4 = " to school"
+str5 = (str3+str4)
+print(str5)
+
+#length of an string
+str6 = "i am a student"
+str7 = " i am persuing engineering"
+print(len(str7))
+print(len(str6))
